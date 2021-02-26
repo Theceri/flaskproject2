f646d2f218daa948d4e1daf8de769056c563867d
Paul Theceri <paul.theceri@gmail.com>
2021-02-26 19:45:11 +0300

    redirects now working

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def render_child():
-    return render_template('child.html')
+def redirect_login():
+    # return render_template('child.html')
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        fname = request.form['first_name']
+        lname = request.form['last_name']
+        print(fname, lname)
